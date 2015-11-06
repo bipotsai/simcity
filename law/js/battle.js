a517979b790d96b68e49d38a55db0f2d20a0c0e5
lai <mrelk168@gmail.com>
2015-11-06 13:24:03 +0800

    title style

---
+++ law/js/battle.js
@@ -0,0 +1,20 @@
+/**
+ * jQuery.marquee - scrolling text like old marquee element
+ * @author Aamir Afridi - aamirafridi(at)gmail(dot)com / http://aamirafridi.com/jquery/jquery-marquee-plugin
+ */;
+(function($) {
+    $.fn.battle = function(options) {
+        return this.each(function() {
+           var total_widh = $('.title-5').width();
+           var left_percentage = $(this).find('.left').data().percentage
+           var left_width = (total_widh - 140) * (left_percentage * 0.01)
+
+           var right_width = total_widh - (left_width + 140)
+           $(this).find('.left .user .name').width(left_width)
+           $(this).find('.right .user .name').width(right_width)
+
+
+        });
+    };
+
+})(jQuery);
